3d82bd7062c95f7bc23839fbe552aaebf982b557
kristina <k.szabova98@gmail.com>
2020-03-23 00:42:06 +0100

    Get only music data

---
+++ scripts/music_only.py
@@ -0,0 +1,13 @@
+import ijson
+import json
+
+with open("../data/song_lyrics_clean.json") as file:
+    objects = ijson.items(file, "item")
+
+    with open("../data/song_lyrics_music_only.json", 'a') as out_file:
+        out_file.write("[\n")
+        for object in objects:
+            if object["is_music"] == "true":
+                out_file.write(json.dumps(object))
+                out_file.write(",\n")
+        out_file.write("]")
