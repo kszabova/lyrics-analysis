9583eacf018b702770afe9f917fd82ebbf6130ba
kristina <k.szabova98@gmail.com>
2020-03-30 01:36:16 +0200

    Small fix

--- scripts/remove_duplicates.py
+++ scripts/remove_duplicates.py
@@ -10,17 +10,20 @@ with open("../data/song_lyrics_english_only.json") as file:
         out_file.write("[\n")
 
         prev_artist_id = None
-        prev_song_id = None
+        prev_title = None
         duplicates = 0
 
         for object in objects:
-            if object["rg_artist_id"] == prev_artist_id and object["rg_song_id"]   == prev_song_id:
+
+            if object["rg_artist_id"] == prev_artist_id and object["title"] == prev_title:
                 # we have a duplicate, do nothing
                 duplicates += 1
             else:
                 # keep the object
                 out_file.write(json.dumps(object))
                 out_file.write(",\n")
+                prev_artist_id = object["rg_artist_id"]
+                prev_title = object["title"]
 
         out_file.write("]")
         print(duplicates)
