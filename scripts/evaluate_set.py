35c90ab8a65a4cdbe9c425ebbc2b55a8a3d5c0d6
kristina <k.szabova98@gmail.com>
2020-04-06 00:11:15 +0200

    Calculate average score over a set of texts

---
+++ scripts/evaluate_set.py
@@ -0,0 +1,32 @@
+import json
+from lyrics_analysis import evaluator
+
+def evaluate_set(set, metric):
+    """
+    Computes average score of a set.
+    :param set: Path to file with data
+    :param metric: Which metric to use
+    :return: Average of scores
+    """
+    try:
+        with open(set) as src:
+            texts = json.load(src)
+    except Exception as e:
+        raise e
+        exit()
+
+    scores = [metric(text) for text in texts]
+    return sum(scores) / len(scores)
+
+if __name__ == "__main__":
+    random = evaluator.eval_random
+    line_length = evaluator.eval_line_length
+    print("Random evaluation:")
+    print("Songs:\t\t", evaluate_set("../data/cleaned/eval_set_100_lyrics.json", random))
+    print("Shuffled songs:\t", evaluate_set("../data/cleaned/eval_set_100_lyrics_shuffled.json", random))
+    print("Random texts:\t", evaluate_set("../data/cleaned/eval_set_100_random.json", random))
+    print()
+    print("Line length evaluation:")
+    print("Songs:\t\t", evaluate_set("../data/cleaned/eval_set_100_lyrics.json", line_length))
+    print("Shuffled songs:\t", evaluate_set("../data/cleaned/eval_set_100_lyrics_shuffled.json", line_length))
+    print("Random texts:\t", evaluate_set("../data/cleaned/eval_set_100_random.json", line_length))
