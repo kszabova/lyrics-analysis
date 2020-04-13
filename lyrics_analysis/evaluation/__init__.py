2fe2495e05dcea65bcd3c51ccfb2312d28244b74
kristina <k.szabova98@gmail.com>
2020-04-13 17:35:58 +0200

    Rename metrics.py to dummy_metrics.py

--- lyrics_analysis/evaluation/__init__.py
+++ lyrics_analysis/evaluation/__init__.py
@@ -1,5 +1,5 @@
-from .metrics import eval_random
-from .metrics import eval_line_length
+from .dummy_metrics import eval_random
+from .dummy_metrics import eval_line_length
 
 __all__ = [
     eval_random,
