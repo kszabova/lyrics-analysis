e36776da784fb2fa9638e6017ec49e493a373564
kristina <k.szabova98@gmail.com>
2020-04-19 23:16:16 +0200

    Refactor

--- scripts/generate_samples.py
+++ scripts/generate_samples.py
@@ -23,10 +23,7 @@ file_prefix = "eval_set_"
 for size in sizes:
     source = data_dir + "song_lyrics_english_only.json"
     dest = data_dir + file_prefix + str(size) + "_lyrics.json"
-    examples = []
-    for example in lyrics_analysis.sampler.sample_n_songs_from_generator(
-                                            size, create_generator_from_file(source)):
-        examples.append(example)
+    examples = list(lyrics_analysis.sample_n_songs_from_generator(size, create_generator_from_file(source)))
     with open(dest, 'w') as out_file:
         json.dump(examples, out_file)
 print("Finished creating lyrics sets.")
@@ -35,10 +32,7 @@ print("Finished creating lyrics sets.")
 for size in sizes:
     source = data_dir + "song_lyrics_english_only.json"
     dest = data_dir + file_prefix + str(size) + "_lyrics_shuffled.json"
-    examples = []
-    for example in lyrics_analysis.sample_n_songs_from_generator(
-                                    size, create_generator_from_file(source)):
-        examples.append(example)
+    examples = list(lyrics_analysis.sample_n_songs_from_generator(size, create_generator_from_file(source)))
     random.shuffle(examples)
     with open(dest, 'w') as out_file:
         json.dump(examples, out_file)
@@ -48,10 +42,7 @@ print("Finished creating shuffled lyrics sets.")
 for size in sizes:
     source = data_dir + "song_lyrics_non_music.json"
     dest = data_dir + file_prefix + str(size) + "_random.json"
-    examples = []
-    for example in lyrics_analysis.sample_n_songs_from_generator(
-                                    size, create_generator_from_file(source)):
-        examples.append(example)
+    examples = list(lyrics_analysis.sample_n_songs_from_generator(size, create_generator_from_file(source)))
     with open(dest, 'w') as out_file:
         json.dump(examples, out_file)
 print("Finished creating random text sets.")
