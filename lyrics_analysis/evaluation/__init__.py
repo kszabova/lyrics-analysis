0d15a3cbe66d41ea6f7c36cf66d6e6d320aede25
kristina <k.szabova98@gmail.com>
2020-04-19 19:00:13 +0200

    Count content words at the end of lines

--- lyrics_analysis/evaluation/__init__.py
+++ lyrics_analysis/evaluation/__init__.py
@@ -1,11 +1,13 @@
 from .dummy_metrics import eval_random
 from .dummy_metrics import eval_line_length
-from .syntactic_metrics import count_unique_words
 from .syntactic_metrics import proportion_content_words
+from .syntactic_metrics import content_words_line_end
+from .vocab_metrics import count_unique_words
 
 __all__ = [
     eval_random,
     eval_line_length,
-    count_unique_words,
-    proportion_content_words
+    proportion_content_words,
+    content_words_line_end,
+    count_unique_words
 ]
