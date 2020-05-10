61a584b877e58bbbcb77b70d3fc5f8eecda705f6
kristina <k.szabova98@gmail.com>
2020-05-11 00:05:39 +0200

    Fix bug fix

--- lyrics_analysis/evaluation/poetic_metrics.py
+++ lyrics_analysis/evaluation/poetic_metrics.py
@@ -17,7 +17,7 @@ def rhymes(lyrics, rhyme_level=2, max_distance=2, arpabet=ARPABET):
     """
 
     last_words = [word.lower() for word in helpers._get_last_words(lyrics)]
-    if len(last_words == 0):
+    if len(last_words) == 0:
         return 0
 
     base_last_phonemes = helpers._get_last_n_phonemes(last_words, rhyme_level, arpabet)
