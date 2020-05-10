dc49784bfcc092f0e8bdae2e7087e758deecf5d4
kristina <k.szabova98@gmail.com>
2020-05-10 21:55:22 +0200

    Add submodule modifications

--- lyrics_analysis/__init__.py
+++ lyrics_analysis/__init__.py
@@ -1,6 +1,7 @@
 from . import sampler
 from . import evaluation
 from . import helpers
+from . import modifications
 
 from .song import Song
 
---
+++ lyrics_analysis/modifications/__init__.py
@@ -0,0 +1,3 @@
+from .word_replacement import replace_last_words_on_line
+
+__all__ = []
