dde6d45e26e0c16edcc21b12a3ec454e6dbb606e
kristina <k.szabova98@gmail.com>
2020-05-11 00:35:06 +0200

    Add models to web app

---
+++ la_site/comparison/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Song(models.Model):
+
+    artist = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    genre = models.TextChoices('Genre', 'Rap Pop Rock R-B Country')
+    lyrics = models.CharField()
+
+    def __str__(self):
+        return str(self.artist) + ": " + str(self.title)
+
+
+class Score(models.Model):
+
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    rhymes = models.FloatField(default=0)
