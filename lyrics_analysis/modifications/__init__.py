a235fd960d2f9109c79a21127077d85d13c529f7
kristina <k.szabova98@gmail.com>
2020-05-10 22:01:48 +0200

    Add function remove_last_word_on_line

--- lyrics_analysis/modifications/__init__.py
+++ lyrics_analysis/modifications/__init__.py
@@ -1,3 +1,4 @@
 from .word_replacement import replace_last_words_on_line
+from .word_replacement import remove_last_word_on_line
 
 __all__ = []
--- lyrics_analysis/modifications/word_replacement.py
+++ lyrics_analysis/modifications/word_replacement.py
@@ -28,3 +28,24 @@ def replace_last_words_on_line(lyrics, words, probability=0.1):
             new_lyrics.append(line)
 
     return new_lyrics
+
+
+def remove_last_word_on_line(lyrics, probability=0.1):
+    """
+    Removes the last word on line with the given probability.
+    :param lyrics: List of strings
+    :param probability: Probability that the last word will be removed
+    :return: Modified lyrics
+    """
+
+    new_lyrics = []
+    for line in lyrics:
+        rand = random.random()
+        if rand <= probability and len(line) > 0:
+            # split the line into words, remove the last one and join them with spaces
+            new_line = ' '.join(line.split()[:-1])
+            new_lyrics.append(new_line)
+        else:
+            new_lyrics.append(line)
+
+    return new_lyrics
