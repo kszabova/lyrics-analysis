7a2bf36c5974442c21dc4033b14a266650c9e14b
kristina <k.szabova98@gmail.com>
2020-07-11 23:05:00 +0200

    Change submit form to reflect changes in models

--- la_site/comparison/forms.py
+++ la_site/comparison/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Song
 
-class SongForm(forms.ModelForm):
-    class Meta:
-        model = Song
-        fields = ['artist', 'title', 'genre', 'lyrics']
+class SongForm(forms.Form):
+    artist = forms.CharField(label='artist', max_length=200)
+    title = forms.CharField(label='title', max_length=200)
+    genre = forms.ChoiceField(label='genre', choices=[
+        ('RAP', 'rap'),
+        ('POP', 'pop'),
+        ('RCK', 'rock'),
+        ('RNB', 'r-b'),
+        ('CTR', 'country')
+    ])
+    lyrics = forms.CharField(label='lyrics', widget=forms.Textarea)
