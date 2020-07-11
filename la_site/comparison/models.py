89840e5427f45ae0249a807360692179c6f08e84
kristina <k.szabova98@gmail.com>
2020-07-11 19:35:58 +0200

    Add Artist model to django app

--- la_site/comparison/models.py
+++ la_site/comparison/models.py
@@ -10,9 +10,20 @@ lyrics_analysis = importlib.util.module_from_spec(spec)
 sys.modules[spec.name] = lyrics_analysis
 spec.loader.exec_module(lyrics_analysis)
 
+
+class Artist(models.Model):
+
+    artist_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    number_of_songs = models.IntegerField()
+    rhyme_score_avg = models.FloatField(default=0)
+    tfidf_score_avg = models.FloatField(default=0)
+
+
 class Song(models.Model):
 
-    artist = models.CharField(max_length=200)
+    song_id = models.IntegerField(primary_key=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=3, choices=[
         ('RAP', 'rap'),
@@ -36,6 +47,7 @@ class Score(models.Model):
 
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     rhyme_score = models.FloatField(default=0)
+    tfidf_score = models.FloatField(default=0)
 
     def __str__(self):
         return str(self.song)
@@ -44,6 +56,8 @@ class Score(models.Model):
     def create(cls, song):
         lyrics = song.lyrics.splitlines()
         rhyme_score = lyrics_analysis.evaluation.rhymes(lyrics)
+        tfidf_score = lyrics_analysis.evaluation.tfidf(lyrics)
 
-        score = cls(song=song, rhyme_score=rhyme_score)
+        score = cls(song=song, rhyme_score=rhyme_score, tfidf_score=tfidf_score)
         return score
+
